b1c6f1540515ada7998f52bd028a7f7a13895540
DominicDev <s0l0estudi4r@gmail.com>
2025-09-20 23:35:19 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'GESAFFELSTEIN ';
+export const nameSongs = [
+	"LOST IN THE FIRE", /*1*/
+	"PURSUIT", /*2*/
+	"ALEPH", /*3*/
+	"HATE OR GLORY", /*4*/
+	"BLAST OFF", /*5*/
+	"SO BAD", /*6*/
+	"RESET", /*7*/
+	"OBSESSION", /*8*/
+	"DESTINATIONS", /*9*/
+	"ORCK", /*10*/
+	"HARD DREAMS", /*11*/
+	"HELLIFORNIA", /*12*/
+	"FOREVER", /*13*/
+	"DANCE X", /*14*/
+	"A PALÉ", /*15*/
+	"LOST IN THE FIRE (TEASER)", /*16*/
+	"MEMORA", /*17*/
+	"EVER NOW", /*18*/
+	"DOOM", /*19*/
+	"NOVO SONIC SYSTEM", /*20*/
+]
